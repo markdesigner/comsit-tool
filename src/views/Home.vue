d3cb01d609ed5d486ad03738b321ebc7cc94b2ed
Mark <mark.chang@vivotek.com>
2021-08-17 00:10:00 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <CopyTool />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CopyTool from "@/components/CopyTool.vue";
+
+export default {
+  name: "Home",
+  components: {
+    CopyTool,
+  },
+};
+</script>
